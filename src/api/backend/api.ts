7a82f18c4dc50cdb7287bf7ed6702fd7094b9422
OmarLawaty <omarlawatey@gmail.com>
2026-05-07 16:20:09 +0300

    feat: Update gamersCoreAdmin to use axios.create with correct baseURL

--- src/api/backend/api.ts
+++ src/api/backend/api.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const gamersCore = axios.create({ baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, withCredentials: true });
 
-export const gamersCoreAdmin = gamersCore.create({ baseURL: '/admin' });
+export const gamersCoreAdmin = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin`,
+  withCredentials: true,
+});
