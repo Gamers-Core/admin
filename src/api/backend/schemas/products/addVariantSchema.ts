98b046061099a0e64fe09a36b8ed843b53cff5a3
OmarLawaty <omarlawatey@gmail.com>
2026-05-16 02:54:17 +0300

    feat: Refactor addVariantSchema definition for improved readability

--- src/api/backend/schemas/products/addVariantSchema.ts
+++ src/api/backend/schemas/products/addVariantSchema.ts
@@ -3,16 +3,21 @@ import z from 'zod';
 import { localizedSchema } from '../localizedSchema';
 import { MediaByFolder } from '../../types';
 
-export const addVariantSchema = z.object({
-  name: localizedSchema,
-  isActive: z.boolean().optional(),
-  stock: z.number().int().min(0),
-  price: z.number().int().min(0),
-  imageId: z.number().int().min(1),
-  image: z.custom<MediaByFolder<'variant'>>().optional(),
-  costPerItem: z.number().int().min(0),
-  position: z.number().int().min(0),
-  compareAt: z.number().int().min(0).optional(),
-});
+export const addVariantSchema = z
+  .object({
+    name: localizedSchema,
+    isActive: z.boolean().optional(),
+    stock: z.number().int().min(0),
+    price: z.number().int().min(0),
+    imageId: z.number().int().min(1),
+    image: z.custom<MediaByFolder<'variant'>>().optional(),
+    costPerItem: z.number().int().min(0),
+    position: z.number().int().min(0),
+    compareAt: z.number().int().min(0).optional(),
+  })
+  .refine((data) => !data.compareAt || data.compareAt > data.price, {
+    message: 'Compare at price must be greater than price',
+    path: ['compareAt'],
+  });
 
 export type AddVariantSchema = z.infer<typeof addVariantSchema>;
