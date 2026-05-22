8762a15f81f0f11b8cdeb8a4f2beccaff8142fb6
OmarLawaty <omarlawatey@gmail.com>
2026-05-22 08:48:33 +0300

    fix: Adjust layout and styling in ProductPreviewCard for better responsiveness

--- src/components/products/ProductPreviewCard.tsx
+++ src/components/products/ProductPreviewCard.tsx
@@ -16,8 +16,8 @@ export const ProductPreviewCard = ({ variant, className }: ProductPreviewCardPro
 
   return (
     <div className={cn('relative flex justify-between gap-2 p-4 border rounded-lg shadow-sm w-full', className)}>
-      <div className="flex flex-1 items-center gap-4">
-        <div className="size-20">
+      <div className="flex flex-1 items-center gap-2 md:gap-4">
+        <div className="size-14 shrink-0 md:size-20">
           <Media
             media={variant.image}
             alt={`Variant ${variant.id} Image`}
@@ -26,9 +26,9 @@ export const ProductPreviewCard = ({ variant, className }: ProductPreviewCardPro
         </div>
 
         <div className="flex flex-col justify-center h-full gap-1">
-          <h3 className="font-medium text-base">{variant.product.name[defaultLocale]}</h3>
+          <h3 className="font-medium text-base line-clamp-1">{variant.product.name[defaultLocale]}</h3>
 
-          <p className="text-sm text-muted-foreground">{variant.name[defaultLocale]}</p>
+          <p className="text-sm text-muted-foreground line-clamp-1">{variant.name[defaultLocale]}</p>
         </div>
       </div>
 
