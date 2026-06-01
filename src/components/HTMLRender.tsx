47e558b7204af81e493bf5b752b02e9cbb0667b1
OmarLawaty <omarlawatey@gmail.com>
2026-06-01 05:49:33 +0300

    feat: Add Tailwind CSS typography plugin and enhance HTMLRender component styling

--- src/components/HTMLRender.tsx
+++ src/components/HTMLRender.tsx
@@ -1,13 +1,22 @@
 import { memo } from 'react';
 
+import { cn } from '@/lib/utils';
+
 interface HTMLRenderProps {
   html: string;
   className?: string;
 }
 
 export const HTMLRender = (displayName: string) => {
-  const Component = memo(({ html, ...props }: HTMLRenderProps) => (
-    <div dangerouslySetInnerHTML={{ __html: html }} {...props} />
+  const Component = memo(({ html, className, ...props }: HTMLRenderProps) => (
+    <div
+      dangerouslySetInnerHTML={{ __html: html }}
+      className={cn(
+        'prose prose-sm md:prose-base dark:prose-invert max-w-none prose-li:leading-normal prose-p:mt-0 prose-p:mb-0 prose-ul:mb-0 prose-ul:mt-0',
+        className,
+      )}
+      {...props}
+    />
   ));
 
   Component.displayName = displayName;
